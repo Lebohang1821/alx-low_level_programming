a98f55d4f75b2ecbc6d6b09d80430fda4a7369f5
Lebohang1821 <millboylebohang@gmail.com>
2023-09-15 13:00:08 -0700

    task103

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -6,50 +6,47 @@
 
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
- */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+* main - Generates and prints passwords for the crackme5 executable.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
+*/
+int main(int __achar generateCharacter(const char *input, int xorValue) {
+int len = strlen(input);
+int tmp = 0;
 
+for (int x = 0; x < len; x++) {
+tmp += input[x];
+}
 
-char generateCharacter(const char *input, int xorValue) {
-    int len = strlen(input);
-    int tmp = 0;
-    
-    for (int x = 0; x < len; x++) {
-        tmp += input[x];
-    }
-
-    return input[(tmp ^ xorValue) & (CODEX_LENGTH - 1)];
+return input[(tmp ^ xorValue) & (CODEX_LENGTH - 1)];
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <input_string>\n", argv[0]);
-        return 1;
-    }
+if (argc != 2) {
+printf("Usage: %s <input_string>\n", argv[0]);
+return 1;
+}
 
-    char codex[CODEX_LENGTH] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-    char password[8];
-    
-    if (strlen(argv[1]) < 1) {
-        printf("Input string must not be empty.\n");
-        return 1;
-    }
+char codex[CODEX_LENGTH] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+char password[8];
 
-    for (int i = 0; i < 6; i++) {
-        password[i] = generateCharacter(argv[1], 59 + (i * 20));
-    }
+if (strlen(argv[1]) < 1) {
+printf("Input string must not be empty.\n");
+return 1;
+}
 
-    srand(argv[1][0] ^ 14);
-    password[6] = codex[rand() & (CODEX_LENGTH - 1)];
+for (int i = 0; i < 6; i++) {
+password[i] = generateCharacter(argv[1], 59 + (i * 20));
+}
 
-    password[7] = '\0';
+srand(argv[1][0] ^ 14);
+password[6] = codex[rand() & (CODEX_LENGTH - 1)];
 
-    printf("Generated Password: %s\n", password);
+password[7] = '\0';
 
-    return 0;
+printf("Generated Password: %s\n", password);
+
+return 0;
 }
