41a85ff0b5db18c97041fb7a20f92223266a6804
Lebohang1821 <millboylebohang@gmail.com>
2023-10-05 15:02:52 -0700

    task-0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int x;
+	unsigned long int i;
 
-	h_table = malloc(sizeof(hash_table_t));
-	if (h_table == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-	h_table->size = size;
-	h_table->array = malloc(size * sizeof(hash_node_t *));
-	if (h_table->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(h_table);
+		free(hash_table);
 		return (NULL);
 	}
-	for (x = 0; x < size; x++)
-		h_table->array[x] = NULL;
-	return (h_table);
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
