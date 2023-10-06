8975e90ece51a48596034bfc8d9fa182fdef1927
Lebohang1821 <millboylebohang@gmail.com>
2023-10-06 14:10:53 -0700

    task05

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,12 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - It prints the key-value pairs from a hash table
+ * hash_table_print - It prints the k
+ * ey-value pairs from a hash table
  * @ht: A pointer to the hash table to be printed
  *
- * This function iterates through the given hash table and prints its key-value
- * pairs in the format: '<key>: '<value>''. The pairs are enclosed within curly
- * braces and separated by commas, resulting in a readable representation of the
+ * This function iterates through the 
+ * given hash table and prints its key-value
+ * pairs in the format: '<key>: '<value>''. 
+ * The pairs are enclosed within curly
+ * braces and separated by commas, resulting in a 
+ * readable representation of the
  * hash table.
  */
 void hash_table_print(const hash_table_t *ht)
